ae89eac0ba17722c5046b3dac348669bd652c847
Shasuna Lin <162057763+shasunal@users.noreply.github.com>
2025-05-13 16:21:18 -0400

    push

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+require('dotenv').config();
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var postsRouter = require('./routes/posts');
+const {storage }= require('./config/cloudinary');
 
 
 
@@ -24,6 +27,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/posts', postsRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/posts.js
+++ routes/posts.js
@@ -1,1 +1,41 @@
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const { storage } = require('../config/cloudinary');
+const upload = multer({ storage });
+const Post = require('../models/Posts'); // Example Mongoose model
+const { ObjectId } = require("mongodb");
+const { getDb } = require("../config/db");
 
+router.get('/upload', (req, res) => {
+res.render('posts');
+});
+
+
+router.post("/posts", upload.single('image'), async (req, res) => {
+  const db = getDb();
+  const { petsName, petsAge } = req.body;
+
+  //when file is uploaded, cloudinary url
+  if (req.file && req.file.path) {
+    return res.status(400).json({ message: "Image is required" });
+  }
+
+  const newPet = {
+    petsName,
+    petsAge,
+    petsImage: req.file.path, // Cloudinary URL
+    likeCount: 0
+  };
+
+
+  try {
+    const result = await db.collection("petsCollection").insertOne(newPet);
+    res.status(201).json({ message: "Pet added", result });
+  } catch (err) {
+    console.error("POST error:", err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+module.exports = router;
